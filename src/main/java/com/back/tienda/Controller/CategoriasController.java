62e587ee8c181d81670b27ef75ba4e5e22ffa58a
o  | |  o <90270276+Agugodzic@users.noreply.github.com>
2023-02-09 14:17:50 -0300

    migracion datos

--- src/main/java/com/back/tienda/Controller/CategoriasController.java
+++ src/main/java/com/back/tienda/Controller/CategoriasController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/categoriasa")
+@RequestMapping("/categorias")
 public class CategoriasController {
 
     private final CategoriasService categoriasService;
