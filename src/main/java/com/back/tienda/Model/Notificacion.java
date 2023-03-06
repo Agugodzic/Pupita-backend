9bd9bc96e7da33ecda48e8d5c4dbe8c7e9be38a2
o  | |  o <90270276+Agugodzic@users.noreply.github.com>
2023-03-06 16:45:12 -0300

    Update Notificacion.java

--- src/main/java/com/back/tienda/Model/Notificacion.java
+++ src/main/java/com/back/tienda/Model/Notificacion.java
@@ -5,11 +5,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Any;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Notificacion {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long Id;
   private Long id;
   private Boolean live_mode;
   private String type;
