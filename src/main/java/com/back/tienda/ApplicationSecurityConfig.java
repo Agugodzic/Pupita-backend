2cd6ff1ebe9b982df2c35fa37682a89b21b1dc6f
o  | |  o <90270276+Agugodzic@users.noreply.github.com>
2023-03-10 16:59:17 -0300

    -

--- src/main/java/com/back/tienda/ApplicationSecurityConfig.java
+++ src/main/java/com/back/tienda/ApplicationSecurityConfig.java
@@ -80,7 +80,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         cc.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
         cc.setAllowedOrigins(Arrays.asList("/*"));
         cc.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "PUT","PATCH"));
-        cc.addAllowedOrigin("https://pupitapetshop-com-ar.preview-domain.com");
+        cc.addAllowedOrigin("https://pupitapetshop.com.ar");
         cc.setMaxAge(Duration.ZERO);
         cc.setAllowCredentials(Boolean.TRUE);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
